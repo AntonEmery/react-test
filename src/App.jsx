7ea11f85f61123a21477771ce0d2297a7767ccff
AntonEmery <antonemery@gmail.com>
2018-07-19 15:04:41 -0700

    set initial state, returning data from api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ArticlePreview from'./components/ArticlePreview';
 
-class App extends Component {
+class App extends React.Component {
+
+  state = {
+    articleData: []
+  }
+
+  async componentDidMount() {
+    try {
+      const data = await fetch('https://thewirecutter.com/wp-json/wp/v2/posts');
+      const result = await data.json();
+      this.setState({ articleData: result })
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
   render() {
+    const previewContent = this.state.articleData.map(item => {
+      console.log(item);
+      return item;
+    })
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Anton Emery - React Test</h1>
         </header>
-        <p className="App-intro">
-        </p>
+        <ArticlePreview />
       </div>
     );
   }
---
+++ src/components/ArticlePreview.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ArticlePreview extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Title</h3>
+        <p>Excerpt</p>
+        <p>Author</p>
+      </div>
+    )
+  }
+
+}
+
+export default ArticlePreview;
