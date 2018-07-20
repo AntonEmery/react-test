6d6b398a0644237d7f3279e77af549d25cc3d8b3
AntonEmery <antonemery@gmail.com>
2018-07-20 11:01:03 -0700

    added airbnb linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ArticlePreview from'./components/ArticlePreview';
+import ArticlePreview from './components/ArticlePreview';
 
-class App extends React.Component {
-
-  state = {
-    articleData: []
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articleData: [],
+    };
   }
+
 
   async componentDidMount() {
     try {
       const data = await fetch('https://thewirecutter.com/wp-json/wp/v2/posts');
       const result = await data.json();
-      this.setState({ articleData: result })
-    } catch(error) {
-      console.log(error)
+      this.setState({ articleData: result });
+    } catch (error) {
+      console.log(error);
     }
   }
 
   render() {
-    const previewContent = this.state.articleData.map((article, index) => {
-      return <ArticlePreview
-        key={index}
-        title={article.title.rendered}
-        excerpt={article.excerpt.rendered}
-      />
-    })
+    const { articleData } = this.state;
+    const previewContent = articleData.map((article, index) => {
+      return (
+        <ArticlePreview
+          key={index}
+          title={article.title.rendered}
+          excerpt={article.excerpt.rendered}
+        />
+      );
+    });
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Anton Emery - React Test</h1>
+          <h1 className="App-title">
+            Anton Emery - React Test
+          </h1>
         </header>
         {previewContent}
       </div>
--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 function ArticlePreview({ title, excerpt }) {
   return (
     <div className="article-preview">
-      <h3>{title}</h3>
-      <p dangerouslySetInnerHTML={{__html: excerpt}}></p>
+      <h3>
+        {title}
+      </h3>
+      <p dangerouslySetInnerHTML={{__html: excerpt}} />
     </div>
-  )
+  );
 }
 
 export default ArticlePreview;
